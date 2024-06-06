787c4daf3759c8e19935d3b35b5c19cbb84ac414
KamakhyaBansal <bansalkamakhya@gmail.com>
2024-06-06 11:13:16 +0530

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,6 +1,8 @@
+#Install and import required libraries
 %pip install timm
-
-#Visualise multiple bands
+%pip install --quiet pytorch-lightning>=1.4
+%pip install plotly
+%pip install seaborn
 from glob import glob
 %pip install earthpy
 import earthpy as et
@@ -11,13 +13,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.colors import ListedColormap
 
-%pip install plotly
 import plotly.graph_objects as go
 import os
 from rasterio.enums import Resampling
 
-
-%pip install seaborn
 import numpy as np
 import pandas as pd
 import os
@@ -29,7 +28,7 @@ from torch import nn, optim
 from torch.nn import functional as F
 from torch.utils.data import DataLoader, sampler, random_split
 from torchvision import models
-%pip install timm
+
 import timm
 from timm.loss import LabelSmoothingCrossEntropy # This is better than normal nn.CrossEntropyLoss
 
@@ -38,8 +37,6 @@ warnings.filterwarnings("ignore")
 import matplotlib.pyplot as plt
 %matplotlib inline
 import sys
-
-
 import matplotlib.pyplot as plt
 import seaborn as sns
 import torch
@@ -53,13 +50,6 @@ from torchvision import models
 from torchvision.utils import make_grid
 from torch.utils.data import random_split, DataLoader
 from sklearn.model_selection import train_test_split
-
-
-
-
-%pip install --quiet pytorch-lightning>=1.4
-
-
 import random
 import seaborn as sns
 import torch
@@ -130,12 +120,7 @@ torch.backends.cudnn.benchmark = False
 device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 print("Device:", device)
 
-from google.colab import drive
-drive.mount('/content/drive')
-
-#dataset_size = 6000
-
-
+#Divide image into patches of size=patch_size
 def img_to_patch(x, patch_size, flatten_channels=False):
    
     B, C, H, W = x.shape
@@ -148,17 +133,10 @@ def img_to_patch(x, patch_size, flatten_channels=False):
         x = x.flatten(2,4)          # [B, H'*W', C*p_H*p_W]
     return x
 
+#Multihead self attention block
 class AttentionBlock(nn.Module):
 
     def __init__(self, embed_dim, hidden_dim, num_heads, dropout=0.0):
-        """
-        Inputs:
-            embed_dim - Dimensionality of input and attention feature vectors
-            hidden_dim - Dimensionality of hidden layer in feed-forward network
-                         (usually 2-4x larger than embed_dim)
-            num_heads - Number of heads to use in the Multi-Head Attention block
-            dropout - Amount of dropout to apply in the feed-forward network
-        """
         super().__init__()
 
         self.layer_norm_1 = nn.LayerNorm(264)
@@ -172,41 +150,19 @@ class AttentionBlock(nn.Module):
             nn.Linear(hidden_dim, 264),
             nn.Dropout(dropout)
         )
-
-
     def forward(self, x):
-        #print("In attn",x.shape)
         inp_x = self.layer_norm_1(x)
-        #print("In attn",x.shape)
         x = x + self.attn(inp_x, inp_x, inp_x)[0]
-        #print("In attn",x.shape)
-        #x = x + self.linear(x)
-        #print("In attn",x.shape)
         return x
 
-class VisionTransformer(nn.Module):
+class Model(nn.Module):
 
     def __init__(self, embed_dim=256, hidden_dim=512, num_channels=15, num_heads=1, num_layers=1, num_classes=6, patch_size=12, num_patches=36, dropout=0.2):
-        """
-        Inputs:
-            embed_dim - Dimensionality of the input feature vectors to the Transformer
-            hidden_dim - Dimensionality of the hidden layer in the feed-forward networks
-                         within the Transformer
-            num_channels - Number of channels of the input (3 for RGB)
-            num_heads - Number of heads to use in the Multi-Head Attention block
-            num_layers - Number of layers to use in the Transformer
-            num_classes - Number of classes to predict
-            patch_size - Number of pixels that the patches have per dimension
-            num_patches - Maximum number of patches an image can have
-            dropout - Amount of dropout to apply in the feed-forward network and
-                      on the input encoding
-        """
         super().__init__()
 
         self.patch_size = patch_size
 
         # Layers/Networks
-        #self.input_layer_0 = nn.Linear(num_channels, 3)
         self.conv1_1 = nn.Conv2d(in_channels=15, out_channels=64, kernel_size=3, padding=1)
 
         self.conv2_1 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
@@ -227,7 +183,7 @@ class VisionTransformer(nn.Module):
             nn.Conv2d(32,32,3,padding=1),
             nn.Conv2d(32,1,3,padding=1)
         )
-        #self.input_layer = nn.Linear(num_channels, embed_dim)
+        
         self.pool = nn.MaxPool2d(kernel_size=2,stride=2)
         self.conv1 = nn.Conv2d(embed_dim+3,264,3,stride=1,padding=1)
         self.transformer = nn.Sequential(*[AttentionBlock(264, hidden_dim, num_heads, dropout=dropout) for _ in range(num_layers)])
@@ -246,54 +202,32 @@ class VisionTransformer(nn.Module):
 
 
     def forward(self, x):
-        # Preprocess input
-        #print("In ViT",x.shape)
-        #print(x.shape)
-        '''plt.figure()
-        plt.axis('off')
-        plt.imshow(show_rgb(x[0].detach()))'''
-        #VGG
+        #Sequence of convolutions
         x = (self.conv1_1(x))
-
         x = (self.conv2_1(x))
-        '''plt.figure()
-        plt.axis('off')
-        plt.imshow(x[0][3].detach())'''
+        
         x = F.relu(x)
         x = self.maxpool(x)
         x = self.dropout(x)
         x = F.relu(self.conv3_1(x))
         x = (self.conv3_2(x))
-        '''plt.figure()
-        plt.axis('off')
-        plt.imshow(x[0][3].detach())'''
+        
         x = F.relu(x)
         x = self.maxpool(x)
         x = self.dropout(x)
-        #print("Done vgg 256*8*8",x.shape)
-
-        #Inception
+        
+        #Inception block with parallel multi-scale convolutions
         X=self.maxpool(F.relu(self.Iconv1(x)))
-        #print("1",X.shape)
         Y=self.maxpool(F.relu(self.Iconv2(x)))
-        #print("2",Y.shape)
         Z=self.maxpool(F.relu(self.Iconv3(x)))
-        #print("3",Z.shape)
         W= self.maxpool(x)
-        #print("4",X.shape)
         inceptionout = [X,Y,Z,W]
         x = torch.cat(inceptionout,1)
-        #print("Done inception",x.shape)
-        #x = img_to_patch(x, self.patch_size)
-        #print("Patches ",x.shape)
-        '''plt.figure()
-        plt.axis('off')
-        plt.imshow(x[0][3].detach())'''
 
+        #Another convolution to adjust the size
         x = self.dropout(x)
-
         x = F.relu(self.conv1(x))
-        #print(x.shape)
+        
         x = self.pool(x)
         x = self.dropout(x)
 
@@ -301,26 +235,24 @@ class VisionTransformer(nn.Module):
 
         #print("Pool ",x.shape)
         x = x.permute(0,2,1)
+        #Application of attention block
         x = self.transformer(x)
-        #print("Transformer out",x.shape)
-
+        
         # Perform classification prediction
-        #cls = x[:,0,:]
         x = x.flatten()
-        #print(x.shape)
+        #Apply MLP block
         out = self.mlp_head(x)
-        #print("final out",out)
         out = out.unsqueeze(0)
         return out
 
 from sklearn.metrics import f1_score,precision_score,recall_score,confusion_matrix
 
-class ViT(pl.LightningModule):
+class ModelTest(pl.LightningModule):
 
     def __init__(self, model_kwargs, lr):
         super().__init__()
         self.save_hyperparameters()
-        self.model = VisionTransformer(**model_kwargs)
+        self.model = Model(**model_kwargs)
         self.example_input_array = next(iter(train_loader))[0]
         self.val_step_outputs = []        # save outputs in each batch to compute metric overall epoch
         self.val_step_targets = []
@@ -380,7 +312,7 @@ class ViT(pl.LightningModule):
 
 
 def train_model(**kwargs):
-    trainer = pl.Trainer(default_root_dir=os.path.join(CHECKPOINT_PATH, "ViT"),
+    trainer = pl.Trainer(default_root_dir=os.path.join(CHECKPOINT_PATH, "ModelTest"),
                          max_epochs = 180,
                          callbacks=[ModelCheckpoint(save_weights_only=True, mode="max", monitor="val_acc"),
                                     LearningRateMonitor("epoch")])
@@ -389,19 +321,18 @@ def train_model(**kwargs):
 
     # Check whether pretrained model exists. If yes, load it and skip training
 
-    #ckpt_file = os.listdir("/content/drive/MyDrive/Colab Notebooks/ViT/lightning_logs/version_77/checkpoints/")[0]
-    #pretrained_filename = os.path.join("/content/drive/MyDrive/Colab Notebooks/ViT/lightning_logs/version_77/checkpoints/", ckpt_file)
-    pretrained_filename = "/content/drive/MyDrive/Colab Notebooks/ViT/lightning_logs/Acc_69.92/checkpoints/epoch=5-step=56016.ckpt"
+    ckpt_file = os.listdir(CHECKPOINT_PATH)
+    pretrained_filename = os.path.join(CHECKPOINT_PATH, ckpt_file)
     if os.path.isfile(pretrained_filename):
         print(f"Found pretrained model at {pretrained_filename}, Retraining....")
-        model = ViT.load_from_checkpoint(pretrained_filename) # Automatically loads the model with the saved hyperparameters
+        model = ModelTest.load_from_checkpoint(pretrained_filename) # Automatically loads the model with the saved hyperparameters
     else:
         pl.seed_everything(42) # To be reproducable
         print("Training from scratch")
-        model = ViT(**kwargs)
+        model = ModelTest(**kwargs)
 
     trainer.fit(model, train_loader, val_loader)
-    model = ViT.load_from_checkpoint(trainer.checkpoint_callback.best_model_path) # Load best checkpoint after training
+    model = ModelTest.load_from_checkpoint(trainer.checkpoint_callback.best_model_path) # Load best checkpoint after training
 
     # Test best model on validation and test set
     #train_result = trainer.test(model, train_loader, verbose=True)
@@ -415,12 +346,9 @@ def train_model(**kwargs):
     return model, result
 
 
-def show_rgb(msi):
-  return torch.stack((msi[4],msi[3],msi[2]),dim=2)/torch.max(torch.max(torch.max(msi[3]),torch.max(msi[2])),torch.max(msi[1]))
-
-
-
+#Define the checkpoint
 CHECKPOINT_PATH="/content/drive/MyDrive/Colab Notebooks/"
+#Train the model
 model, results = train_model(model_kwargs={
                                 'embed_dim': 256,
                                 'hidden_dim': 512,
@@ -433,36 +361,5 @@ model, results = train_model(model_kwargs={
                                 'dropout': 0.4
                             },
                             lr=3e-4)
-print("ViT results", results)
-
-
-
-
-X_train,Y_train=load_tset1()
-X_val,Y_val=load_valset1()
-X_train=np.array(X_train)
-Y_train=np.array(Y_train)
-X_val=np.array(X_val)
-Y_val=np.array(Y_val)
-print(X_train.shape)
-print(Y_train.shape)
-print(X_val.shape)
-print(Y_val.shape)
-
-y_ser=pd.Series(Y_train)
-print("Training Data:")
-print(y_ser.value_counts())
-print("Testing Data:")
-y_sr=pd.Series(Y_val)
-print(y_sr.value_counts())
-
-model.model.conv1
-
-activation = {}
-def get_activation(name):
-    def hook(model, input, output):
-        activation[name] = output.detach()
-    return hook
-
-
+print("Model results", results)
 
