3d2d702c816f848dd133dd4b2a6d99d280065f73
KamakhyaBansal <bansalkamakhya@gmail.com>
2024-06-06 11:01:34 +0530

    Update Gradcam.py

--- Gradcam.py
+++ Gradcam.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
+#Define function for heatmap calculation
 def grad_cam(model, image, layer_name):
 
     image = np.expand_dims(image, axis=0)
@@ -19,43 +20,33 @@ def grad_cam(model, image, layer_name):
     return heatmap
 
 
-#Gradcam
+#Install required modules
 !pip install grad_cam
 !pip install torchinfo
 
-
-
-
-
+#Load the image for deriving gradcam
 import cv2
 imagePath = '/content/drive/MyDrive/Data/METER_ML/CAFOs/val_CAFOs/34.75969091_-78.83480443.tif'
 image_ar = rio.open(imagePath).read(out_shape=(15,72,72),resampling=Resampling.bilinear)
 
 input_tensor = torch.nan_to_num(torch.Tensor(image_ar))
 
-
+#Function for visualising RGB bands of multi-spectral images
 def show_rgb(msi):
   return torch.stack((msi[4],msi[3],msi[2]),dim=2)/torch.max(torch.max(torch.max(msi[3]),torch.max(msi[2])),torch.max(msi[1]))
 
 plt.imshow(show_rgb(input_tensor))
 plt.axis('off')
 
-from pytorch_grad_cam import GradCAM, HiResCAM, ScoreCAM, GradCAMPlusPlus, AblationCAM, XGradCAM, EigenCAM, FullGrad
-from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
-from pytorch_grad_cam.utils.image import show_cam_on_image
-
-
+#Select the target layer from model which has to be visualized
 target_layers = [model.model.conv1_1]
 
+#Derive heatmap
 cam = GradCAM(model=model, target_layers=target_layers)
-
 targets = [ClassifierOutputTarget(0)]
-
 grayscale_cam = cam(input_tensor.unsqueeze(0), targets=targets)
-'''
-visualization = show_cam_on_image(input_tensor.numpy(), torch.Tensor(grayscale_cam).permute(1,2,0).numpy(), use_rgb=True)
-plt.imshow(visualization)'''
 
+#Visualize heatmap with the original image
 plt.imshow(show_rgb(input_tensor))
 plt.imshow(grayscale_cam[0],alpha=0.7)
 plt.axis('off')
